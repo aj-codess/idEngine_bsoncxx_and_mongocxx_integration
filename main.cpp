82ce107f5f39920aff854514edce93a93d8e3faa
aj-codess <jagyei339@gmail.com>
2025-03-14 13:13:30 +0000

    Enviromental setup

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/coroutine/all.hpp>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello fucked up world"<<endl;
+    return 0;
+}
